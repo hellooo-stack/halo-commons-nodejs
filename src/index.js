3aca3e61b6b83634f355fcd0e08eac7b5b23f0dd
Jeb.Wang <jeb.wannng@gmail.com>
2022-12-24 00:26:57 +0800

    optimize: optimize export objs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const hellooo_commons = {
-    ...require('./excel'),
+const excel = {
+    ...require('./excel')
+}
+
+const utils = {
     ...require('./utils')
+}
+
+const allInOne = {
+    ...excel,
+    ...utils
+}
+
+const hellooo_commons = {
+    excel,
+    utils,
+    ...allInOne
 };
 
 module.exports = hellooo_commons;
