ac72be6e9192696b6567956c7a0988a374ea28ad
Jeb.Wang <jeb.wannng@gmail.com>
2022-12-23 15:10:50 +0800

    add contents contentsAsJSON

--- src/excel/reader.js
+++ src/excel/reader.js
@@ -1,6 +1,13 @@
 const XLSX = require('xlsx');
 const _ = require('lodash');
 
+/**
+ * return rows in the specific sheet, each row will be as an item in the resulting array
+ *
+ * @param filePath file path of the workbook
+ * @param sheetIndex default to 0
+ * @returns {unknown[]|*[]} array containing the rows, each row is an array: [['col1 of row1', 'col2 of row1'], ['col1 of row2', 'col2 of row2']]
+ */
 function rows(filePath, sheetIndex) {
 
     sheetIndex = sheetIndex ? sheetIndex : 0;
@@ -22,19 +29,49 @@ function rows(filePath, sheetIndex) {
 /**
  * return headers of specific sheet
  *
- * @param filePath
+ * @param filePath file path of the workbook
  * @param sheetIndex default to 0
- * @returns {string[]|*[]}
+ * @returns {string[]|*[]} array containing the headers
  */
 function headers(filePath, sheetIndex) {
     sheetIndex = sheetIndex ? sheetIndex : 0;
 
     const parsedRows = rows(filePath, sheetIndex);
-    if (_.isEmpty(rows)) {
+    if (_.isEmpty(parsedRows)) {
         return [];
     }
 
     return parsedRows[0];
+}
+
+function contents(filePath, sheetIndex) {
+    sheetIndex = sheetIndex ? sheetIndex : 0;
+
+    const parsedRows = rows(filePath, sheetIndex);
+//    the first row is header,
+    if (_.size(parsedRows) <= 1) {
+        return [];
+    }
+//    so we need to remove the first row
+    return _.takeRight(parsedRows, parsedRows.length - 1);
+}
+
+function contentsAsJSON(filePath, sheetIndex) {
+
+    sheetIndex = sheetIndex ? sheetIndex : 0;
+
+    const workbook = XLSX.readFile(filePath);
+    const sheet = workbook.Sheets[workbook.SheetNames[sheetIndex]];
+//    将工作表转换为JSON格式：
+//      1. 从第二行开始，一行一个item，其中item的key为第一行对应的列，如果第一行对应的列不存在，则为_EMPTY_*
+//      2. 表格内无数据，或只有一行数据时，不会解析成JSON，即：jsonData为空
+    const rowsAsJSON = XLSX.utils.sheet_to_json(sheet);
+//    当excel表格内的sheet内容为空（一行数据都没有）的时候，返回空数组
+    if (_.isEmpty(rowsAsJSON)) {
+        return [];
+    }
+
+    return rowsAsJSON;
 }
 
 
