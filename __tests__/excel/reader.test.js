75d3f49eb359d8ba7d54c22f744d2f5507ce83ad
Jeb.Wang <jeb.wannng@gmail.com>
2022-12-25 00:06:21 +0800

    :art: Reformat dir structure

---
+++ __tests__/excel/reader.test.js
@@ -0,0 +1,32 @@
+//const XLSX = require('xlsx');
+//const _ = require('lodash');
+//const rows = require('../../src/excel');
+//console.log(rows);
+//// const reader = require('../../src/excel/reader');
+//
+//// reader.rows()
+//
+//// test('should return rows in the specific sheet', () => {
+////     const filePath = 'generated.xlsx';
+////     const sheetIndex = 0;
+////     const expectedRows = [['col1 of row1', 'col2 of row1'], ['col1 of row2', 'col2 of row2']];
+////
+////     const result = rows(filePath, sheetIndex);
+////     expect(result).toEqual(expectedRows);
+//// });
+//
+//describe('rowsFromExcel', () => {
+//    it('should return the rows from the specified sheet in the Excel file', () => {
+//        // Arrange
+//        const filePath = 'path/to/test/file.xlsx';
+//        const expectedRows = [      ['col1 of row1', 'col2 of row1'],
+//            ['col1 of row2', 'col2 of row2'],
+//        ];
+//
+//        // Act
+//        const rows = rowsFromExcel(filePath);
+//
+//        // Assert
+//        expect(rows).toEqual(expectedRows);
+//    });
+//});
