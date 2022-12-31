e66730a5282b9a0c3933c5c259fc34635dcf80e6
Jeb.Wang <jeb.wannng@gmail.com>
2023-01-01 00:21:47 +0800

    temp commit

--- __tests__/http/axios.test.js
+++ __tests__/http/axios.test.js
@@ -36,3 +36,49 @@ test('t2', async () => {
         });
 });
 
+test('t3', async () => {
+
+    // Add a request interceptor
+    axios.interceptors.request.use(function (config) {
+        console.log('before: ', config);
+        // Do something before request is sent
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+// Add a response interceptor
+    axios.interceptors.response.use(function (response) {
+        console.log('response: ', response);
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    }, function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    });
+
+    await axios.get('https://www.baidu.com')
+        .then(function (response) {
+//            console.log(response.data);
+//            console.log(response.status);
+//            console.log(response.statusText);
+//            console.log(response.headers);
+//            console.log(response.config);
+
+        });
+});
+
+//export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+test('request google', async () => {
+    const result = await axios.get('https://www.google.com', {
+        proxy: {
+            protocol: 'http',
+            host: '127.0.0.1',
+            port: 7890
+        }
+    });
+    console.log(result);
+})
--- src/http/axios.js
+++ src/http/axios.js
@@ -2,43 +2,133 @@
  * @author: Jeb.Wang
  * @date: 2022/12/22 19:20
  */
+const axios = require('axios');
+const cookie = require('cookie');
 
-function get(url, params, header, cookie, proxy) {
+function request(config) {
+    return axios.request(config);
+}
+
+function get(url, params = {}) {
+
+    if (this instanceof AxiosRequest) {
+        return this.instance.get(url, {
+            params: params
+        });
+    } else {
+        return axios.get(url, {
+            params: params
+        });
+    }
+}
+
+function getWithHeader(url, headers = {}, params = {}) {
+    if (this instanceof AxiosRequest) {
+        return this.instance.get(url, {
+            headers: headers,
+            params: params
+        });
+    } else {
+        return axios.get(url, {
+            headers: headers,
+            params: params
+        });
+    }
+}
+
+function getWithCookie(url, cookies = {}, params = {}) {
 
 }
 
-function getAndWriteToFile(url, filePath, params, header, cookie, proxy) {
+function getWithProxy(url, proxy, params = {}) {
+    if (this instanceof AxiosRequest) {
+        return this.instance.get(url, {
+            proxy: proxy,
+            params: params
+        });
+    } else {
+        return axios.get(url, {
+            proxy: proxy,
+            params: params
+        });
+    }
+}
+
+function post(url, data) {
 
 }
 
-function getWithParams(url, params) {
+function postWithHeader(url, data, headers) {
 
 }
 
-function getWithHeader(url, header) {
+function postWithCookie(url, data, cookies) {
 
 }
 
-function getWithCookie(url, cookie) {
+function postWithProxy(url, data, proxy) {
 
 }
 
-function getWithProxy(url, proxy) {
+// todo
+// https://stackoverflow.com/questions/55374755/node-js-axios-download-file-stream-and-writefile
+function getAndPipeTo(url, filePath, config) {
 
 }
 
-function postJSON(url, json) {
+function createAxiosRequest(baseURL = '', config = {}) {
+    if (config.baseURL) {
+        delete config.baseURL;
+    }
 
+    if (config.url) {
+        delete config.url;
+    }
+
+    config = {
+        baseURL,
+        ...config
+    };
+
+    return new AxiosRequest(config);
+}
+function AxiosRequest(config) {
+    this.instance = axios.create(config);
 }
 
-function postJSONWithHeader(url, json, header) {
+AxiosRequest.prototype.get = get;
+AxiosRequest.prototype.getWithHeader = getWithHeader;
+AxiosRequest.prototype.getWithCookie = getWithCookie;
+AxiosRequest.prototype.getWithProxy = getWithProxy;
+AxiosRequest.prototype.post = post;
+AxiosRequest.prototype.postWithHeader = postWithHeader
+AxiosRequest.prototype.postWithCookie = postWithCookie;
+AxiosRequest.prototype.postWithProxy = postWithProxy;
+AxiosRequest.prototype.getAndPipeTo = getAndPipeTo;
 
-}
 
-function postJSONCookie(url, json, cookie) {
+//module.exports = {
+//    request,
+//    get,
+//    getWithHeader,
+//    getWithCookie,
+//    getWithProxy,
+//    post,
+//    postWithHeader,
+//    postWithCookie,
+//    postWithProxy,
+//    getAndPipeTo,
+//    createAxiosRequest
+//}
 
-}
+//(async () => {
+//    const result = await get('https://www.baidu.com');
+////    console.log('result: ', result);
+////
+//    const axiosRequest = createAxiosRequest('http://localhost:8381');
+//    const result1 = await axiosRequest.get('/members/api/member/');
+//    console.log('result1: ', result1);
+//})();
 
-function postJSONProxy(url, json, proxy) {
-
-}
+const setCookie = cookie.serialize('1','2','3','5');
+console.log(setCookie);
