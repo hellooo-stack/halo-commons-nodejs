2ad529185ec39876580fd8601aa6ff51926641ff
Jeb.Wang <jeb.wannng@gmail.com>
2023-01-01 18:32:36 +0800

    :adhesive_bandage: export http-util.js and string-util.js from utils package

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,9 @@ const utils = {
     ...require('./crypto-util'),
     ...require('./datetime-util'),
     ...require('./file-util'),
+    ...require('./http-util'),
     ...require('./random-util'),
+    ...require('./string-util'),
     ...require('./thread-util')
 };
 
